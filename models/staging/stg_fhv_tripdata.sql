944314a77ca1150fd0c9ffd0375661ff47690428
Mark Graham <mgraham1309@yahoo.co.uk>
2022-02-15 17:50:51 +0000

    fhv added

---
+++ models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,29 @@
+{{ config(materialized='view') }}
+
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
+  from {{ source('staging','fhv_tripdata') }}
+)
+select  
+    -- Identifiers
+    {{ dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    dispatching_base_num as dispatch_base,
+    Affiliated_base_number as affiliated_base,	
+    case when cast (PULocationID as Integer) = 0 then 265 else cast (PULocationID as Integer) end as pickup_locationid,
+    case when cast (DOLocationID as Integer) = 0 then 265 else cast (DOLocationID as Integer) end as dropoff_locationid,
+
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+
+    -- trip info
+    case when SR_Flag = 1 then 'Yes' else 'No' end as shared_ride_indicator
+
+from tripdata
+where rn=1
+
+{% if var('is_test_run', default=true) %}
+    limit 100
+{% endif %}
